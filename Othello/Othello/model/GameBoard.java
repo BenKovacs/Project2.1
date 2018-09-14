7a93039255cd3b5c5c53bb604ce9d541aa92e68f
thieskeloo <thijsvanloo4@gmail.com>
2018-09-14 16:24:37 +0200

    Cleaned up a few small things

--- Othello/Othello/model/GameBoard.java
+++ Othello/Othello/model/GameBoard.java
@@ -1,13 +1,12 @@
 package model;
 
 import gui.RightPanel;
-
 import static model.Constants.*;
 
 public class GameBoard {
 	private int width;
 	private int height;
-	private int[][] bs;
+	private int[][] board;
 	private int enemy;
 	//Game state
 	public static int turn;
@@ -17,27 +16,26 @@ public class GameBoard {
 		this.width = width;
 		this.height = height;
 
-		bs = new int[width][height];
+		board = new int[width][height];
 
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
-				bs[x][y] = -1; // default gray
+				board[x][y] = -1; // default gray
 			}
 		}
 		turn = WHITE;
 		enemy = BLACK;
-		bs[3][3] = WHITE;
-		bs[3][4] = BLACK;
-		bs[4][3] = BLACK;
-		bs[4][4] = WHITE;
+		board[3][3] = WHITE;
+		board[3][4] = BLACK;
+		board[4][3] = BLACK;
+		board[4][4] = WHITE;
 		//starting game position
 	}
-
 
 	public void flipDisc(int x, int y){
 
 		if(isValidMove(x,y)){
-			bs[x][y] = turn;
+			board[x][y] = turn;
 			changeTurn();
 			//return true;
 		} else{
@@ -46,54 +44,52 @@ public class GameBoard {
 		}
 	}
 
-
-
 	public boolean isInsideBoard(int x, int y) {
-		return x >= 0 && x <= width && y >= 0 && y <= height;
+		return x >= 0 && x < width && y >= 0 && y < height;
 	}
-
 
 	public boolean isValidMove(int x, int y) {
-		return bs[x][y] == EMPTY && checkFlip(x,y);
+		return board[x][y] == EMPTY && checkFlips(x,y);
 	}
 
-	public boolean checkFlip(int x, int y) {
+	public boolean checkFlips(int x, int y) {
 	    boolean valid = false;
 
 	    for (int i=x-1; i<=x+1; i++){
             for (int j=y-1; j<=y+1; j++) {
-                try {
-                    if (isInsideBoard(i, j) && bs[i][j] == enemy)
-                        if (checkDirection(x, y, i, j)) {
-                            valid = true;
-                        }
-                } catch(Exception e) {}
+                if (isInsideBoard(i, j)){
+                    if (board[i][j] == enemy) {
+						if (checkDirection(x, y, i, j)) {
+							valid = true;
+						}
+					}
+                }
             }
 	    }
 	    return valid;
 	}
 
 	public boolean checkDirection(int x, int y, int i, int j) {
-		if (bs[x+(i-x)][y+(j-y)] == EMPTY)
+		if (board[x+(i-x)][y+(j-y)] == EMPTY) {
 			return false;
-		else if (bs[x+(i-x)][y+(j-y)] == turn)
+		}
+		else if (board[x+(i-x)][y+(j-y)] == turn) {
 			return true;
-		else if (isInsideBoard(x+(i-x)*2,y+(j-y)*2))
-			if (checkDirection(x+(i-x),y+(j-y),x+(i-x)*2,y+(j-y)*2)) {
-				bs[x + (i - x)][y + (j - y)] = turn;
+		}
+		else if (isInsideBoard(x+(i-x)*2,y+(j-y)*2)) {
+			if (checkDirection(x + (i - x), y + (j - y), x + (i - x) * 2, y + (j - y) * 2)) {
+				board[x + (i - x)][y + (j - y)] = turn;
 				return true;
-			}
-			else
-				return false;
-		else
-			return false;
+			} else {return false;}
+		}
+		else {return false;}
 	}
 
+	public int getSquareType(int x, int y) { return board[x][y]; }
 
-
-	public int getSquareType(int x, int y) { return bs[x][y]; }
-
-	public int[][] getboard(){ return bs ; }
+	public int[][] getboard(){
+		return board ;
+	}
 
 	public int getHeight() {
 		return height;
@@ -108,7 +104,7 @@ public class GameBoard {
 	}
 
 	public void changeTurn(){
-		if(turn == WHITE) {
+		if (turn == WHITE) {
 			turn = BLACK;
 			enemy = WHITE;
 		}
@@ -118,6 +114,7 @@ public class GameBoard {
 		}
 		showValidMoves();
 	}
+
 	public void showValidMoves(){
 
 	}
