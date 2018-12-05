28aea945144d19d9dd0c4620872e49801dde66d7
SaosinHax <flux.math@gmail.com>
2018-12-05 14:14:57 +0100

    Fixed AI vs AI

--- Othello/Othello/gui/MainApp.java
+++ Othello/Othello/gui/MainApp.java
@@ -1,6 +1,5 @@
 package gui;
 
-
 import model.GameBoard;
 import model.Constants;
 import model.player.*;
@@ -10,26 +9,25 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class MainApp implements ActionListener {
-	
+
 	private static MainApp mainApp;
-	
+
 	public static MainApp getSingleton() {
 		return mainApp;
 	}
 
 	private JFrame frame;
 	public GameBoard gameBoard;
-	
+
 	public JFrame getFrame() {
 		return frame;
 	}
 
-	//--PANELS
-	//right RightPanel
+	// --PANELS
+	// right RightPanel
 	private RightPanel rPanel;
-	//left LeftPanel
+	// left LeftPanel
 	private BoardPanel boardPanel;
 	private Settings settings;
 
@@ -53,7 +51,7 @@ public class MainApp implements ActionListener {
 	/**
 	 * Create the application.
 	 */
-	private MainApp(){
+	private MainApp() {
 
 		frame = new JFrame();
 		frame.setBounds(100, 100, 927, 473);
@@ -61,7 +59,7 @@ public class MainApp implements ActionListener {
 
 		initialize();
 	}
-	
+
 	public void reset() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 927, 473);
@@ -76,88 +74,70 @@ public class MainApp implements ActionListener {
 	 */
 
 	private void initialize() {
-		
 
 		settings = new Settings();
-		// The settings Dialog is Modal so this thread will pause after setting it to be visible until it is set invisible by the dialog.
+		// The settings Dialog is Modal so this thread will pause after setting
+		// it to be visible until it is set invisible by the dialog.
 		settings.setVisible(true);
-		if (settings.isCancelSelected()){
+		if (settings.isCancelSelected()) {
 			System.exit(0);
 		}
 
-		//create the gameBoard/panel
-		gameBoard = new GameBoard(8,8);
+		// create the gameBoard/panel
+		gameBoard = new GameBoard(8, 8);
 
-		//set the layout
-		frame.getContentPane().setLayout(new GridLayout(1,2));
+		// set the layout
+		frame.getContentPane().setLayout(new GridLayout(1, 2));
 
 		rPanel = new RightPanel();
 
 		boardPanel = new BoardPanel(gameBoard, rPanel);
-		frame.getContentPane().add(boardPanel, 1,0);
+		frame.getContentPane().add(boardPanel, 1, 0);
 
-		//Create AIs or human players
-<<<<<<< HEAD
+		// Create AIs or human players
 		int turn = Constants.WHITE;
-		Player[] playerArray = new Player[settings.getNumberOfAIPlayers()+settings.getNumberOfHumanPlayers()];
-		for (int i = 0; i < settings.getNumberOfHumanPlayers(); i++){
-			playerArray[i] = new HumanPlayer(boardPanel, turn);
-			//Change color
-			if(turn == Constants.WHITE) {
-				turn = Constants.BLACK;
-			} else {
-				turn = Constants.WHITE;
-			}
-//			playerArray[i] = new MonteCarloTreeSearch(boardPanel);
-		}
-
-		for (int i = settings.getNumberOfHumanPlayers(); i < playerArray.length; i++) {
-			AIPlayer ai = new AIPlayer(boardPanel,turn);
-			playerArray[i] = ai;
-			ai.start();
-			if(turn == Constants.WHITE) {
-				turn = Constants.BLACK;
-			} else {
-				turn = Constants.WHITE;
-			}
-=======
 
 		Player[] playerArray = new Player[settings.getNumPlayers()];
-		if (settings.getPlayer1().equalsIgnoreCase("human")){
-			playerArray[0] = new HumanPlayer(boardPanel);
-		}else if(settings.getPlayer1().equalsIgnoreCase("minmax")){
-			playerArray[0] = new MinMaxPlayer(boardPanel);
-		}else if(settings.getPlayer1().equalsIgnoreCase("greedy")){
-			playerArray[0] = new GreedyPlayer(boardPanel);
-		}else if(settings.getPlayer1().equalsIgnoreCase("random")){
-			playerArray[0] = new AIPlayer(boardPanel);
-		}else if(settings.getPlayer1().equalsIgnoreCase("mcts")){
-	//		playerArray[0]; //= new TODO add correct code : MCTS(boardPanel);
-			}
-
-
-		if (settings.getPlayer2().equalsIgnoreCase("human")){
-			playerArray[1] = new HumanPlayer(boardPanel);
-		}else if(settings.getPlayer2().equalsIgnoreCase("minmax")){
-			playerArray[1] = new MinMaxPlayer(boardPanel);
-		}else if(settings.getPlayer2().equalsIgnoreCase("greedy")){
-			playerArray[1] = new GreedyPlayer(boardPanel);
-		}else if(settings.getPlayer2().equalsIgnoreCase("random")){
-			playerArray[1] = new AIPlayer(boardPanel);
-		}else if(settings.getPlayer2().equalsIgnoreCase("mcts")){
-			//playerArray[1] //= new TODO add correct code : MCTS(boardPanel);
->>>>>>> origin/master
+		if (settings.getPlayer1().equalsIgnoreCase("human")) {
+			playerArray[0] = new HumanPlayer(boardPanel, turn);
+		} else if (settings.getPlayer1().equalsIgnoreCase("minmax")) {
+			MinMaxPlayer ai = new MinMaxPlayer(boardPanel, turn);
+			playerArray[0] = ai;
+		} else if (settings.getPlayer1().equalsIgnoreCase("greedy")) {
+			GreedyPlayer ai = new GreedyPlayer(boardPanel, turn);
+			playerArray[0] = ai;
+			
+		} else if (settings.getPlayer1().equalsIgnoreCase("random")) {
+			AIPlayer ai = new AIPlayer(boardPanel, turn);
+			ai.start();
+			playerArray[0] = ai;
+		} else if (settings.getPlayer1().equalsIgnoreCase("mcts")) {
+			// playerArray[0]; //= new TODO add correct code : MCTS(boardPanel);
 		}
 
+		turn = Constants.BLACK;
+		if (settings.getPlayer2().equalsIgnoreCase("human")) {
+			playerArray[1] = new HumanPlayer(boardPanel, turn);
+		} else if (settings.getPlayer2().equalsIgnoreCase("minmax")) {
+			playerArray[1] = new MinMaxPlayer(boardPanel, turn);
+		} else if (settings.getPlayer2().equalsIgnoreCase("greedy")) {
+			playerArray[1] = new GreedyPlayer(boardPanel, turn);
+		} else if (settings.getPlayer2().equalsIgnoreCase("random")) {
+			AIPlayer ai = new AIPlayer(boardPanel, turn);
+			playerArray[1] = ai;
+			ai.start();
+		} else if (settings.getPlayer2().equalsIgnoreCase("mcts")) {
+			// playerArray[1] //= new TODO add correct code : MCTS(boardPanel);
+		}
 
-		for(int i = 0; i < playerArray.length; i++) {
+		for (int i = 0; i < playerArray.length; i++) {
 			System.out.println("Player " + i + " is: " + playerArray[i]);
+			
 		}
 		gameBoard.setPlayers(playerArray);
 
 		rPanel.setPreferredSize(new Dimension(10, 30));
-		frame.getContentPane().add(rPanel, 0,1);
-
+		frame.getContentPane().add(rPanel, 0, 1);
 
 	}
 
@@ -166,4 +146,3 @@ public class MainApp implements ActionListener {
 		// TODO Auto-generated method stub
 	}
 }
-
